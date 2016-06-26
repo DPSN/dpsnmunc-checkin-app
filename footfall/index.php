1980789429e585ee7fb23382ecf76519b057634b
SwG Ghosh <codecrafts.cf@icloud.com>
2016-06-26 17:04:32 +0530

    footfall syntax fix

--- footfall/index.php
+++ footfall/index.php
@@ -1,26 +1,26 @@
 <?php
 include_once("../db.php");
 
-$sql = "SELECT * FROM `participants`;"
+$sql = "SELECT * FROM `participants`;";
 $res = mysqli_query($db, $sql);
 $number = mysqli_num_rows($res);
 
-$sql = "SELECT * FROM `participants` WHERE `type` = 'indidel';"
+$sql = "SELECT * FROM `participants` WHERE `type` = 'indidel';";
 $res = mysqli_query($db, $sql);
 $indidel = mysqli_num_rows($res);
 
-$sql = "SELECT * FROM `participants` WHERE `type` = 'schooldel';"
+$sql = "SELECT * FROM `participants` WHERE `type` = 'schooldel';";
 $res = mysqli_query($db, $sql);
 $schooldel = mysqli_num_rows($res);
 
-$sql = "SELECT * FROM `participants` WHERE `type` = 'dpsndel';"
+$sql = "SELECT * FROM `participants` WHERE `type` = 'dpsndel';";
 $res = mysqli_query($db, $sql);
 $dpsndel = mysqli_num_rows($res);
 ?>
 <!doctype html>
 <html>
     <head>
-        <title>DPSNMUNC'16 CHECK IN APPLICATION RECIEPT</title>
+        <title>DPSNMUNC'16 FOOTFALL</title>
         <meta name="viewport" content="width=device-width, user-scalable=yes">
         <link rel="stylesheet" href="../style2.css" type="text/css" />
         <link rel="icon" href="http://swghosh.cu.cc/favicon.png" type="image/png" />
