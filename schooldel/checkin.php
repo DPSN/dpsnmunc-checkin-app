99db87bf97d81404da1e520625c4c3c1be81e234
SwG Ghosh <codecrafts.cf@icloud.com>
2016-06-26 21:15:51 +0530

    schooldel/checkin.php added

---
+++ schooldel/checkin.php
@@ -0,0 +1,51 @@
+<?php
+include_once("../db.php");
+
+function form_error() {
+    die(header('Location: ../wrong.html'));
+}
+
+//Form Validation
+if(isset($_POST['name']) == false || isset($_POST['number']) == false || isset($_POST['institution']) == false || isset($_POST['committee']) == false) {
+    form_error();
+}
+
+if(empty($_POST['name']) || empty($_POST['number']) || empty($_POST['institution']) || empty($_POST['committee'])) {
+    form_error();
+}
+
+$number = htmlspecialchars($_POST['number']);
+$institution = htmlspecialchars($_POST['institution']);
+$type = 'schooldel';
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>DPSNMUNC'16 CHECK IN APPLICATION RECIEPT</title>
+        <meta name="viewport" content="width=device-width, user-scalable=yes">
+        <link rel="stylesheet" href="../style2.css" type="text/css" />
+        <link rel="icon" href="http://swghosh.cu.cc/favicon.png" type="image/png" />
+        <meta name="theme-color" content="#336">
+    </head>
+    <body>
+      <h1>DPSNMUNC'16 check-in successful!<br>Reciept</h1>
+      <ul>
+        <li><span>DPS Newtown Delegate</span></li>
+        <li><span><?php echo $institution; ?></span></li>
+        <li><span><?php echo $number; ?></span></li>
+        <?php
+        for($i = 0; $i < sizeof($_POST['name']); $i++) {
+          $name = $_POST['name'][$i];
+          $committee = $_POST['committee'][$i];
+          $sql = "INSERT INTO `participants` (`name`,`number`,`institution`,`committee`,`type`) VALUES ('$name','$number','$institution','$committee','$type');";
+          if(mysqli_query($db, $sql) == false) {
+              die("Form Data Submission Error.");
+          }
+          echo '<li><span>'.$name.'</span><li><span>'.$committee.'</span><li>\n';
+        }
+        ?>
+      </ul>
+      <p>Welcome aboard!</p>
+      <p><small><a href="/">another check-in</a></small></p>
+    </body>
+</html>
